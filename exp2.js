e351ca002d407600fe419068d8b9922dec9d69ac
rdhruvi231992 <rdhruvi231992@gmail.com>
2020-10-12 16:17:53 +0800

    Add files via upload

---
+++ exp2.js
@@ -0,0 +1,4 @@
+<p id="a"></p>
+var d = new Date();
+
+document.getElementById("a").innerHTML =d.getTime();
---
+++ planet.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("form");
+
+
+function submitEvent(event) {
+   
+    event.preventDefault();
+    let input = document.getElementById("search");
+    let resultDiv = document.getElementById("results");
+    console.log("value:"+input.value);
+    
+
+    //https://www.swapi.tech/api/planets/8
+
+    fetch("https://www.swapi.tech/api/people/?name=r2")
+        .then(function(response) {
+            return response.json()
+            
+
+        })
+        .then(function(result) {
+            for(let i =0; i< result.results.length;i++){
+                let char = result.results[i].properties;
+                let chars = result.results[i].properties;
+                let characterDiv = document.createElement("div");
+                let html= "<h2> planets: <h2>"
+                
+                html += "<ul>"
+               
+                html += "<li>planets: " + char.homeworld + "</li>"
+                html += "<li>planets: " + chars.name + "</li>"
+                
+                
+              
+                characterDiv.innerHTML =html;
+                console.log(resultDiv);
+                resultDiv.appendChild(characterDiv);
+                characterDiv.innerHTML =html;
+                console.log(resultDiv);
+                resultDiv.appendChild(characterDiv);
+            }
+        });
+            console.log("after fetch:");
+             
+        
+            fetch 
+
+        }
+
+        form.addEventListener("submit", submitEvent);
